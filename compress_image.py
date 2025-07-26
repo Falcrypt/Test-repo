4201fed0f6c19e91bfab3bd667ca783af7ac1d77
Falcrypt <philiptesimbo@gmail.com>
2025-07-26 16:57:52 +0000

    Update compress_image.py

--- compress_image.py
+++ compress_image.py
@@ -1,1 +1,8 @@
+from PIL import Image
+import os
 
+img = Image.open("images/sample.jpg")
+
+img.save("compressed/sample_compressed.jpg", "JPEG", quality=30)
+
+print("Image compressed successfully!")
